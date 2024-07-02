2981e859ba5344ece745e42b6bcb8b85483ef2df
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-02 18:45:03 +0530

    edit-profile page complete

---
+++ app/edit-event-details.tsx
@@ -0,0 +1,17 @@
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import tw from "twrnc";
+
+import SafeView from "@/components/SafeView";
+import Title from "@/components/Title";
+import LoadingModal from "@/components/LoadingModal";
+
+const EditEventDetails = () => {
+  return (
+    <SafeView>
+      <Text>EditEventDetails</Text>
+    </SafeView>
+  );
+};
+
+export default EditEventDetails;
--- app/edit-profile.tsx
+++ app/edit-profile.tsx
@@ -1,15 +1,22 @@
-import { View, Text, TextInput, Pressable, Image } from "react-native";
+import { View, Text, TextInput, Pressable, Image, Alert } from "react-native";
 import React, { useCallback, useState } from "react";
 import tw from "twrnc";
 import { MaterialIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import * as SecureStore from "expo-secure-store";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { ZodError } from "zod";
 
 import SafeView from "@/components/SafeView";
 import Header from "@/components/Header";
 import Title from "@/components/Title";
-import { useUser } from "@/hooks/useUser";
+import { UserType, useUser } from "@/hooks/useUser";
+import LoadingModal from "@/components/LoadingModal";
+import { router } from "expo-router";
 
 const EditProfile = () => {
-  const { user } = useUser();
+  const { user, setUser } = useUser();
 
   const [name, setName] = useState(user?.name);
   const [phoneNumber, setPhoneNumber] = useState(user?.phoneNumber);
@@ -26,9 +33,63 @@ const EditProfile = () => {
     []
   );
 
-  const pickImage = useCallback(() => {}, []);
+  const pickImage = useCallback(async () => {
+    const res = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      base64: true,
+      allowsEditing: true,
+    });
+
+    if (res.canceled) {
+      return;
+    }
+    const base64 = `data:image/png;base64,${res.assets?.[0].base64}`;
+    setImage(base64);
+  }, []);
+
+  const { mutate: handleEditProfile, isPending } = useMutation({
+    mutationKey: ["edit-profile"],
+    mutationFn: async () => {
+      const token = await SecureStore.getItemAsync("token");
+      if (!token) {
+        throw new Error("Authenication failed. Please login again!");
+      }
+      if (
+        name === user?.name &&
+        image === user?.image &&
+        phoneNumber === user?.phoneNumber
+      ) {
+        return { head: user, isChanged: false } as {
+          head: UserType;
+          isChanged: boolean;
+        };
+      }
+      const { data } = await axios.post(
+        `${process.env.EXPO_PUBLIC_API_URL}/edit-profile`,
+        { token, name, phoneNumber, image }
+      );
+
+      return { head: data.head, isChanged: true } as {
+        head: UserType;
+        isChanged: true;
+      };
+    },
+    onSuccess: async (data) => {
+      if (data.isChanged) {
+        await SecureStore.setItemAsync("user", JSON.stringify(data.head));
+        setUser(data.head);
+      }
+      Alert.alert("Success", "Profile edited sucessfully", [
+        {
+          text: "Ok",
+          onPress: router.back,
+        },
+      ]);
+    },
+  });
   return (
     <SafeView>
+      <LoadingModal isVisible={isPending} />
       <Header showBackButton />
 
       <Title>Edit Profile</Title>
@@ -75,6 +136,8 @@ const EditProfile = () => {
 
         <Pressable
           style={tw`bg-violet-600 w-[80%] items-center py-3 justify-center rounded-lg`}
+          onPress={() => handleEditProfile()}
+          disabled={isPending}
         >
           <Text style={tw`text-white text-base font-semibold`}>
             Edit Profile
